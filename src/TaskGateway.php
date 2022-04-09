bcdd96b0c67f348271b0baf16293d8b723211e08
SkipEveryLunch <harima.yasuhiro@outlook.jp>
2022-04-09 22:26:44 +0900

    fixed GET /tasks so that it can return boolean

--- src/TaskGateway.php
+++ src/TaskGateway.php
@@ -9,7 +9,12 @@
               FROM task
               ORDER BY name";
       $stmt = $this->conn->query($sql);
-      return $stmt->fetchAll(PDO::FETCH_ASSOC);
+      $data = [];
+      while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        $row["is_completed"] = (bool) $row["is_completed"];
+        $data[] = $row;
+      }
+      return $data;
     }
   }
 ?>
