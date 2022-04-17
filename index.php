08e904a1116bcf29f8770dc2d83b3f295ca6fd80
skipeverylunch <skipeverylunch@outlook.jp>
2022-04-17 17:55:22 +0900

    implemented getByAPI method

--- index.php
+++ index.php
@@ -18,6 +18,22 @@
   $db = new Database($_ENV["DB_HOST"],$_ENV["DB_NAME"],$_ENV["DB_USER"],$_ENV["DB_PASS"]);
   $task_gateway = new TaskGateway($db);
   $controller = new TaskController($task_gateway);
+  if(empty($_SERVER["HTTP_X_API_KEY"])){
+    http_response_code(400);
+    echo json_encode([
+      "message"=>"missing API key"
+    ]);
+    exit;
+  }
+  $api_key = $_SERVER["HTTP_X_API_KEY"];
+  $user_gateway = new UserGateway($db);
+  if($user_gateway->getByAPI($api_key)===false){
+    http_response_code(401);
+    echo json_encode([
+      "message"=>"Invalid API key"
+    ]);
+    exit;
+  }
   header("content-type:application/json; charset:UTF-8");
   $controller->processRequest($method,$id);
 ?>
